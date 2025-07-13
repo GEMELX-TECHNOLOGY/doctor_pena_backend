4e52fe090621650c4d4d750bdc515758e98d0ef6
Michelle Ortega <caro.orte0207@gmail.com>
2025-07-12 21:57:43 -0600

    Delete local storage

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const { storage } = require("../config/cloudinary");
-const upload = multer({ storage });
+const { uploadDocuments } = require("../config/cloudinary"); 
 const controller = require("../controllers/documentController");
 const verifyAndRenewToken = require("../middlewares/authMiddleware");
 
 router.use(verifyAndRenewToken);
 
-router.post("/upload", upload.single("file"), controller.uploadDocument);
+router.post("/upload", uploadDocuments.single("file"), controller.uploadDocument);
 router.get("/patient/:id", controller.getByPatient);
 router.delete("/:id", controller.deleteDocument);
 router.get("/:id/view", controller.viewAndMarkAsRead);
