0d0e35773d2678a98d44dfe95d1cfe0dad48bd65
Michelle Ortega <caro.orte0207@gmail.com>
2025-07-16 10:19:41 -0600

    Tensor flow Fix parametres

--- utils/iaAnalisis.js
+++ utils/iaAnalisis.js
@@ -1,22 +1,30 @@
 const tf = require("@tensorflow/tfjs");
 
-// Simulación: valores normales promedio para entrenamiento
-const normalRanges = tf.tensor2d([[70, 98, 36.5]]);
+// Valores "normales" basados en rangos clínicos medios
+const normalMeans = tf.tensor2d([[80, 97, 37]]); // 80 bpm, 97% SpO₂, 37°C
 
-// Función para calcular "distancia" del valor actual al normal
 function analizarSignosVitales({ heart_rate, oxygenation, temperature }) {
-	const input = tf.tensor2d([[heart_rate, oxygenation, temperature]]);
-	const distancia = tf.sub(input, normalRanges).abs().sum().arraySync();
+  const input = tf.tensor2d([[heart_rate, oxygenation, temperature]]);
+  const diff = tf.sub(input, normalMeans).abs();
 
-	let riesgo = "normal";
-	if (distancia > 15) {
-		riesgo = "riesgo moderado";
-	}
-	if (distancia > 30) {
-		riesgo = "riesgo alto";
-	}
+  // Podemos también examinar cada señal individualmente
+  const [dHR, dSpO2, dTemp] = diff.arraySync()[0];
 
-	return riesgo;
+  const distanciaTotal = diff.sum().arraySync();
+
+  let riesgo = "normal";
+  if (distanciaTotal > 15) riesgo = "riesgo moderado";
+  if (distanciaTotal > 30) riesgo = "riesgo alto";
+
+  return {
+    riesgo,
+    detalles: {
+      frecuencia_cardiaca: dHR,
+      oxigenacion: dSpO2,
+      temperatura: dTemp,
+      distanciaTotal
+    }
+  };
 }
 
 module.exports = { analizarSignosVitales };
