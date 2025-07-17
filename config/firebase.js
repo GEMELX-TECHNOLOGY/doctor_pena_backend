d7f61d94df4c8431925ef25ac312ffb25d58687f
Michelle Ortega <caro.orte0207@gmail.com>
2025-07-17 10:11:34 -0600

    Firebase Change

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,13 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-service-account.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 try {
 	admin.initializeApp({
 		credential: admin.credential.cert(serviceAccount),
 	});
-
 	const db = admin.firestore();
 	console.log("✅ Conectado a Firebase Firestore correctamente");
-
 	module.exports = { admin, db };
 } catch (error) {
 	console.error("❌ Error al conectar a Firebase Firestore:", error);
